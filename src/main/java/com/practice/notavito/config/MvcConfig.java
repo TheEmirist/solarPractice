eb546bda6496372428c5fa9e357edcc645484472
emirist3000 <emirist3000@gmail.com>
2022-07-13 15:34:17 +0300

    Create an Unsecured Web Application

--- src/main/java/com/practice/notavito/config/MvcConfig.java
+++ src/main/java/com/practice/notavito/config/MvcConfig.java
@@ -1,17 +1,17 @@
-// package com.practice.notavito.config;
+package com.practice.notavito.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class MvcConfig implements WebMvcConfigurer {
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
 
-// 	public void addViewControllers(ViewControllerRegistry registry) {
-// 		registry.addViewController("/login").setViewName("login");
-//         registry.addViewController("/home").setViewName("home");
-// 		registry.addViewController("/").setViewName("home");
-// 		registry.addViewController("/hello").setViewName("hello");
-// 	}
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/addAd").setViewName("addAd");
+	}
 
-// }
+}
